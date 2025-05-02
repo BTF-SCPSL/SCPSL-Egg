0ee72446a4d84b7e6c199290ff13abe3c8c419f7
Narin <65040937+Narin4ik@users.noreply.github.com>
2025-05-02 11:16:35 +0300

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # Begin of installation
 echo "###############################################################"
@@ -8,9 +10,48 @@ echo "#         Created by Waenara -- waenara.dev@gmail.com         #"
 echo "#                      Fixed by Narin                         #"
 echo "###############################################################"
 
-# Install dependencies
+# Install base dependencies
 apt-get update
-apt-get install -y curl wget unzip libicu-dev lib32gcc-s1 rsync
+apt-get install -y \
+    curl \
+    wget \
+    unzip \
+    libicu-dev \
+    lib32gcc-s1 \
+    rsync \
+    ca-certificates \
+    gnupg \
+    lsb-release \
+    apt-transport-https
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+# Add Microsoft package repository and install .NET 9.0 prerequisites
+echo "###############################################################"
+echo "#            Adding Microsoft package repository             #"
+echo "###############################################################"
+curl -sSL https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb \
+     -o packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+# Install all dependencies including .NET SDK 9.0
+echo "###############################################################"
+echo "#          Installing dependencies and .NET SDK 9.0          #"
+echo "###############################################################"
+apt-get update
+apt-get install -y \
+    sudo \
+    curl \
+    wget \
+    unzip \
+    adduser \
+    python3 \
+    python3-pip \
+    python3-venv \
+    libicu-dev \
+    lib32gcc-s1 \
+    dotnet-sdk-9.0
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
@@ -31,34 +72,60 @@ cd /mnt/server/.bin/SteamCMD
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 
 # Download SCP:Secret Laboratory Dedicated Server
-./steamcmd.sh +force_install_dir /mnt/server/.bin/SCPSLDS +login anonymous +app_update 996560 -beta "$BETA_NAME" $( [ "$BETA_PASSWORD" != "none" ] && echo "-betapassword $BETA_PASSWORD" ) validate +quit
+./steamcmd.sh +force_install_dir /mnt/server/.bin/SCPSLDS \
+    +login anonymous \
+    +app_update 996560 -beta "$BETA_NAME" \
+    $( [ "${BETA_PASSWORD:-none}" != "none" ] && echo "-betapassword $BETA_PASSWORD" ) \
+    validate +quit
 
-# Download Exiled
-if [ "$EXILED_INSTALLATION" -ne 0 ]; then
+# Download Exiled (dynamic latest asset)
+if [ "${EXILED_INSTALLATION:-0}" -ne 0 ]; then
+    echo "###############################################################"
+    echo "#                     Installing Exiled                     #"
+    echo "###############################################################"
     mkdir -p /mnt/server/.bin/ExiledInstaller
     cd /mnt/server/.bin/ExiledInstaller
-    wget https://github.com/ExMod-Team/EXILED/releases/latest/download/Exiled.Installer-Linux
+
+    ASSET_URL=$(curl -s https://api.github.com/repos/ExMod-Team/EXILED/releases/latest \
+        | grep '"browser_download_url":' \
+        | grep 'Exiled.Installer-Linux' \
+        | head -n1 \
+        | cut -d '"' -f4)
+
+    if [ -z "$ASSET_URL" ]; then
+        echo "Error: could not find Exiled.Installer-Linux asset." >&2
+        exit 1
+    fi
+
+    wget -q "$ASSET_URL" -O Exiled.Installer-Linux
     chmod +x Exiled.Installer-Linux
-    ./Exiled.Installer-Linux --path /mnt/server/.bin/SCPSLDS --appdata /mnt/server/.config/ --exiled /mnt/server/.config/ $([ "$EXILED_INSTALLATION" -eq 2 ] && echo --pre-releases)
+
+    ./Exiled.Installer-Linux \
+      --path /mnt/server/.bin/SCPSLDS \
+      --appdata /mnt/server/.config/ \
+      --exiled /mnt/server/.config/ \
+      $([ "${EXILED_INSTALLATION}" -eq 2 ] && echo --pre-releases)
 fi
 
 # Install Discord bot
-if [ "$SCPDISCORD_INSTALLATION" -eq 1 ]; then
+if [ "${SCPDISCORD_INSTALLATION:-0}" -eq 1 ]; then
+    echo "###############################################################"
+    echo "#                   Installing SCPDiscord                   #"
+    echo "###############################################################"
     mkdir -p /mnt/server/.bin/SCPDiscord
     cd /mnt/server/.bin/SCPDiscord
-    wget https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscordBot_Linux
+    wget -q https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscordBot_Linux
     chmod +x SCPDiscordBot_Linux
     mkdir -p /mnt/server/.config/SCPDiscord
 
-    mkdir -p /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/
-    cd /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/
-    wget https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/dependencies.zip
-    unzip -o dependencies.zip -d /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/
-    rm -f dependencies.zip
-    rm -f /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/SCPDiscord.dll
-    wget https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscord.dll
+    mkdir -p "/mnt/server/.config/SCP Secret Laboratory/PluginAPI/plugins/global/"
+    cd "/mnt/server/.config/SCP Secret Laboratory/PluginAPI/plugins/global/"
+    wget -q https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/dependencies.zip
+    unzip -o dependencies.zip -d .
+    rm -f dependencies.zip SCPDiscord.dll
+    wget -q https://github.com/KarlOfDuty/SCPDiscord/releases/latest/download/SCPDiscord.dll
 else
-    rm /mnt/server/.config/'SCP Secret Laboratory'/PluginAPI/plugins/global/SCPDiscord.dll
+    rm -f "/mnt/server/.config/SCP Secret Laboratory/PluginAPI/plugins/global/SCPDiscord.dll"
 fi
 
 # End of installation
